169b7ff5022091a13c89ad03ebc7a49311ceb601
Jamie0331 <31392353+Jamie0331@users.noreply.github.com>
2019-02-14 20:50:38 -0500

    Update testcharacter.py

--- Bomberman/group28/testcharacter.py
+++ Bomberman/group28/testcharacter.py
@@ -8,24 +8,14 @@ from colorama import Fore, Back
 import random
 
 class TestCharacter(CharacterEntity):
-    def look_for_empty_cell(self, wrld):
-        # List of empty cells
-        cells = []
-        # Go through neighboring cells
-        for dx in [-1, 0, 1]:
-            # Avoid out-of-bounds access
-            if ((self.x + dx >= 0) and (self.x + dx < wrld.width())):
-                for dy in [-1, 0, 1]:
-                    # Avoid out-of-bounds access
-                    if ((self.y + dy >= 0) and (self.y + dy < wrld.height())):
-                        # Is this cell walkable?
-                        if not wrld.wall_at(self.x + dx, self.y + dy):
-                            cells.append((dx, dy))
-                                # All done
-        return cells
 
     def do(self, wrld):
-        safe = self.look_for_empty_cell(wrld)
-        # Pick a move at random
-        (dx, dy) = random.choice(safe)
-        self.move(dx, dy)
+        exit = [0, 0]
+        self_x = wrld.me(self).x
+        self_y = wrld.me(self).y
+        for i in range(wrld.width()):
+            for j in range(wrld.height()):
+                if wrld.exit_at(i, j):
+                    exit = [i, j]
+        next_step = astar.op_path([self_x,self_y],exit,wrld)
+        self.move(-self_x+next_step[0], -self_y+next_step[1])
