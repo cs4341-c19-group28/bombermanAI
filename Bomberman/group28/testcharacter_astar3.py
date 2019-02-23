a7c6a10c70ee4df47e5d8726521b525a6e1f726a
niko1499 <nxgamarra@gmail.com>
2019-02-22 20:05:25 -0500

    working on avoidmon2

--- Bomberman/group28/testcharacter_astar3.py
+++ Bomberman/group28/testcharacter_astar3.py
@@ -32,11 +32,20 @@ class PriorityQueue():
     # for inserting an element in the queue
     def put(self, data, priority):
         self.queue.append((priority, data))
-        self.queue.sort()#TODO put items in queue more inteligently if we have time
+        print("ADDING TO QUEUE", (priority, data))
+        self.queue.sort()  # TODO put items in queue more inteligently if we have time
 
     def get(self):
         if not self.empty():
             return self.queue.pop(0)
+
+        else:
+            print("ERROR: QUEUE EMPTY")
+
+    def get_last(self):
+        if not self.empty():
+            print("QUEUE", self.queue)
+            return self.queue.pop(self.size)
 
         else:
             print("ERROR: QUEUE EMPTY")
@@ -51,7 +60,10 @@ class TestCharacter(CharacterEntity):
     def distance(self, a, b):
         (x1, y1) = a
         (x2, y2) = b
-        return abs(x1 - x2) + abs(y1 - y2)
+        d = abs(x1 - x2) + abs(y1 - y2)
+        # d=math.sqrt(math.pow(abs(x1 - x2),2) + math.pow(abs(y1 - y2),2))
+        print("distance", a, b, d)
+        return d
 
     # returns all valid neighbors
     def get_neighbors(self, wrld, x, y):
@@ -80,7 +92,7 @@ class TestCharacter(CharacterEntity):
                 for dy in range(-radius, radius):
                     # Avoid out-of-bounds access
                     if ((y + dy >= 0) and (y + dy < wrld.height())):
-                        print("searching cell,",x+dx,y+dy)
+                        print("searching cell,", x + dx, y + dy)
                         if wrld.monsters_at(x + dx, y + dy):
                             print("returining ture")
                             return True
@@ -92,7 +104,7 @@ class TestCharacter(CharacterEntity):
         for dx in range(-radius, radius):
             # Avoid out-of-bounds access
             if ((x + dx >= 0) and (x + dx < wrld.width())):
-                for dy in range (-radius, radius):
+                for dy in range(-radius, radius):
                     # Avoid out-of-bounds access
                     if ((y + dy >= 0) and (y + dy < wrld.height())):
                         if wrld.monsters_at(x + dx, y + dy):
@@ -142,12 +154,11 @@ class TestCharacter(CharacterEntity):
         if self.monseter_search(graph, self.x, self.y, 3):
             mon_at = self.monseter_search_2(graph, self.x, self.y, 3)
             relative_mon = (mon_at[0] - self.x, mon_at[1] - self.y)
-            init_dest = (-1*relative_mon[0], -1*relative_mon[1])
+            init_dest = (-1 * relative_mon[0], -1 * relative_mon[1])
             if init_dest[0] != 0:
-                init_dest = (init_dest[0]/abs(init_dest[0]), init_dest[1])
+                init_dest = (init_dest[0] / abs(init_dest[0]), init_dest[1])
             if init_dest[1] != 0:
-                init_dest = (init_dest[0], init_dest[1]/abs(init_dest[1]))
-			
+                init_dest = (init_dest[0], init_dest[1] / abs(init_dest[1]))
 
             if (self.x + init_dest[0]) < 0:
                 init_dest = (init_dest[0] + 1, init_dest[1])
@@ -162,9 +173,28 @@ class TestCharacter(CharacterEntity):
         safe = self.look_for_empty_cell(graph)
         (dx, dy) = random.choice(safe)
         return (self.x + dx, self.y + dy)
-                
+        # Returns x, y coordinate that goes away from monster
 
-    #function to place a bomb but also save the predicted explosion location
+    def avoid_mon2(self, graph, radius):
+        print("avoiding mon2")
+        if self.monseter_search(graph, self.x, self.y, radius):
+            monster_loc = self.monseter_search_2(graph, self.x, self.y, radius)
+            safe = self.look_for_empty_cell(graph)
+            frontier = PriorityQueue()
+            # frontier.put((self.x, self.y), 0)
+            for loc in safe:
+                new_loc = (self.x + loc[0], self.y + loc[1])
+
+                frontier.put(new_loc, -1*(self.distance(new_loc, monster_loc)) )
+
+            current_cost, current_loc = frontier.get()
+
+            return (current_loc)
+        else:
+            print("ERROR: No monster found")
+            return(self.x,self.y)
+
+    # function to place a bomb but also save the predicted explosion location
     def smart_place_bomb(self, x, y, fuse_time):
         if (self.fuse < 0):
             self.fuse = fuse_time + 1
@@ -176,21 +206,21 @@ class TestCharacter(CharacterEntity):
             for j in range(-4, 5):
                 self.explosion_loc.append((x, y + j))
 
-    #function to check if a cell will explode next turn
+    # function to check if a cell will explode next turn
     def will_explode(self, x, y):
         if (self.fuse <= 2) and self.fuse != -1:
             if (x, y) in self.explosion_loc:
                 return True
         return False
 
-    #counts down the fuse, must be called each turn
+    # counts down the fuse, must be called each turn
     def update_fuse(self):
         if (self.fuse >= -1):
             self.fuse = self.fuse - 1
         if (self.fuse == -2):
-            self.explosion_loc.clear()#bug fix for list not clearing properly
+            self.explosion_loc.clear()  # bug fix for list not clearing properly
 
-    #return a list of neighbors that are valid moves and don't kill you
+    # return a list of neighbors that are valid moves and don't kill you
     def look_for_empty_cell(self, wrld):
         # List of empty cells
         cells = []
@@ -212,31 +242,32 @@ class TestCharacter(CharacterEntity):
     def do(self, wrld):
         dx = 0
         dy = 0
+        monster_search_rad = 2
 
-        if self.fuse>0:
-            jc_ABOMB=True
+        if self.fuse > 0:
+            jc_ABOMB = True
         else:
-            jc_ABOMB=False
+            jc_ABOMB = False
 
-        if self.monseter_search(wrld, self.x, self.y, 3):  # behavior when the fuse is running
+        if self.monseter_search(wrld, self.x, self.y, monster_search_rad):  # behavior when the fuse is running
             print("--AVOIDING MONSTER")
             # safe = self.look_for_empty_cell(wrld)
             # (dx, dy) = random.choice(safe)
-            target = self.avoid_mon(wrld)
-            print("Target = ",target)
+            target = self.avoid_mon2(wrld, monster_search_rad)
+            print("Target = ", target)
             path, cost = self.astar(wrld, (self.x, self.y), target)
             step_list = [target]
-        elif(jc_ABOMB):
+        elif (jc_ABOMB):
             print("--RANDOM")
             if self.will_explode(self.x + dx, self.y + dy):  # never step into an explosion
-                 safe = self.look_for_empty_cell(wrld)
-                 (dx, dy) = random.choice(safe)
+                safe = self.look_for_empty_cell(wrld)
+                (dx, dy) = random.choice(safe)
         else:
             print("--A*")
-            path, cost = self.astar(wrld, (self.x, self.y), (wrld.exitcell))#do A*
+            path, cost = self.astar(wrld, (self.x, self.y), (wrld.exitcell))  # do A*
             step_list = [wrld.exitcell]
 
-        # TODO remove debug prints in final sumbission
+            # TODO remove debug prints in final sumbission
             print("path", path)
 
         print("fuse", self.fuse)
@@ -244,16 +275,17 @@ class TestCharacter(CharacterEntity):
         print("current loc", self.x, " ", self.y)
         # TODO remove debug prints in final sumbission
 
-
-        #turn dict into a list
-        step_list = [wrld.exitcell]
+        # turn dict into a list
+        # step_list = [wrld.exitcell]
 
         # Go until we find None as our "came from"
         ok = True
+
         while ok:
             try:
                 # Find where frontmost node came from
                 came_from = path[step_list[0]]
+                print("working",came_from)
                 # If that position is None (IE the first move), break out of the while loop
                 if came_from is None:
                     break
@@ -283,8 +315,6 @@ class TestCharacter(CharacterEntity):
         # place a bomb if pathing through a wall
         if wrld.wall_at(self.x + dx, self.y + dy):
             self.smart_place_bomb(self.x, self.y, wrld.bomb_time)
-
-        
 
         # Done in avoid_mon
         # if self.fuse > 0 and not self.monseter_search(wrld, self.x, self.y, 2):
