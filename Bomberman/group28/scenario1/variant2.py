0fefc05b97d5b3879efa2d341451a84685cb2044
niko1499 <nxgamarra@gmail.com>
2019-02-28 19:25:50 -0500

    expectimax bug fixes

--- Bomberman/group28/scenario1/variant2.py
+++ Bomberman/group28/scenario1/variant2.py
@@ -10,7 +10,7 @@ from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
 sys.path.insert(1, '../groupNN')
-from testcharacter_astar5 import TestCharacter
+from expectimax_search import TestCharacter
 
 # Create the game
 random.seed() # TODO Change this if you want different random choices
